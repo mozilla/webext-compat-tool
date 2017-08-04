f44e2efa3946fddbc12b5d6f59309a6482a6145c
Matt Claypotch <thepotch@gmail.com>
2017-08-04 11:31:13 -0700

    specify response types

--- server.js
+++ server.js
@@ -65,10 +65,16 @@ app.get('/test/:id', function(req, res) {
     .then(function (r) {
       return fs.readFile(path.join(__dirname, 'run.html'));
     })
-    .then(res.end.bind(res))
+    .then(file => {
+      res.type('html');
+      res.end(file);
+    })
     .catch(e => {
       fs.readFile(path.join(__dirname, '404.html'))
-        .then(res.end.bind(res))
+        .then(file => {
+          res.type('html');
+          res.end(file);
+        })
         .catch(e => res.end(e));
     });
 });
