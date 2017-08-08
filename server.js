28e57655c0846ab9389ee2aa2d1020ea8924138a
Matt Claypotch <thepotch@gmail.com>
2017-08-08 09:59:14 -0700

    fix header check

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const store = require('./lib/store');
 
 if (!process.env.DEVELOPMENT) {
   app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https') {
+    if (req.header['x-forwarded-proto'] !== 'https') {
       res.redirect(`https://${req.header('host')}${req.url}`);
       res.header('Content-Security-Policy', "default-src 'none'; connect-src 'self'; img-src 'self'; script-src 'self' use.fontawesome.com 'unsafe-eval' cdn.fontawesome.com; style-src 'self' code.cdn.mozilla.net use.fontawesome.com; font-src code.cdn.mozilla.net use.fontawesome.com");
       res.header('Strict-Transport-Security', 'max-age=63072000');
