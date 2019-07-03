3b0b3115cfe1f0cb06d44be42c97620751e4381d
Philipp Kewisch <mozilla@kewis.ch>
2019-07-03 13:14:22 +0200

    Lint zip/crx files directly through addons-linter (#31)
    This allows addons-linter to handle the crx parsing, which means we will get CRX3 support
    automatically

--- uncrx.js
+++
@@ -1,8 +0,0 @@
-const unzip = require("unzip-crx");
-const path = require("path");
-
-const crxFile = process.argv[process.argv.length - 1];
-
-unzip(crxFile, './output').then(() => {
-  console.log("Successfully unzipped your crx file..");
-});
